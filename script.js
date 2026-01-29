3b774181e5544805034a1bf2e3b486970d6f4fe3
Lujan Maciel <lujanmaciel2@gmail.com>
2026-01-29 11:03:04 -0300

    Fix: corregido error de ortografía y actualizado enlace de CV

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () =>{
     menuIcon.classList.toggle('bx-x')
     navbar.classList.toggle('active');
-=======
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menuIcon.onclick = () =>{
-    menuIcon.classList.toggle('bx-x')
-    navbar.classList.toggle('active');
->>>>>>> master
 }
